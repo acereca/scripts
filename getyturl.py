d1a335467c5a7b366a2d5fbaadda17741a5234b6
acereca <tigerauge96@live.de>
2017-05-19 17:20:28 +0200

    add urlfinding script

---
+++ getyturl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# This Script will find the stream url
+#
+# Author: Patrick Nisble
+
+from bs4 import BeautifulSoup
+import urllib.request as rq
+import sys
+import subprocess
+
+channel_url = "https://www.youtube.com/user/MagicalMusicChannel"
+
+r = ["streamlink", "--player=mpv --volume=50", r"youtube.com"] + sys.argv[1:]
+
+p = rq.urlopen(channel_url).read()
+soup = BeautifulSoup(p,'html.parser')
+links = soup.find_all("a")
+for elem in links:
+    if isinstance(elem.get('title'), str) and "Magical Music Radio" in elem.get("title"):
+        r[2] += elem.get('href')
+        break
+
+print(r)
+
+subprocess.call(r)
